5d10e6858b8b8910310869ffd7ce482cfd82ed63
Adx <deagbowale@gmail.com>
2019-08-09 22:27:05 +0000

    UI improvement to login and signup flow

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res){
 
 // REGISTER FORM
 router.get("/register", function(req,res){
-	res.render('register');
+	res.render('register', {page: 'register'});
 });
 
 //  handle signup logic
@@ -25,11 +25,11 @@ router.post("/register", function(req, res){
 	var newUser = new User({username: req.body.username});
 	User.register(newUser, req.body.password, function(err, user){
 		if(err){
-			req.flash("error", err.message);
-			return res.redirect("/register");
+			console.log(err);
+			return res.render("register", {error: err.message});
 		}
 		passport.authenticate("local")(req, res, function(){
-			req.flash("success", "Welcome to Yelcamp " + user.username);
+			req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
 			res.redirect("/campgrounds");
 		});
 	});
@@ -40,7 +40,7 @@ router.post("/register", function(req, res){
 // show login form
 
 router.get("/login", function(req, res){
-	res.render('login');
+	res.render('login', {page: 'login'});
 });
 
 router.post("/login", passport.authenticate("local", 
